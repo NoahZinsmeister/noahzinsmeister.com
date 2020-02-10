2136a0cb51d8380e3c2e8ee1e5617be60261250b
Noah Zinsmeister <noahwz@gmail.com>
2020-02-10 16:38:20 -0500

    try again

--- ipfs-deploy.mjs
+++ ipfs-deploy.mjs
@@ -1,11 +1,13 @@
 import path from 'path'
-const request = require('request-promise-native')
-const pinata = require('@pinata/sdk')(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY)
+import request from 'request-promise-native'
+import pinataSDK from '@pinata/sdk'
 
 const DOMAIN = 'noahzinsmeister.com'
 const PINATA_NAME = 'noahzinsmeister.com'
 
 const DNSLINK_REGEX = new RegExp(/dnslink=\/ipfs\/(.*)/)
+
+const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY)
 
 async function main() {
   // get the existing dnslink record from zeit
@@ -22,7 +24,6 @@ async function main() {
       return record
     })
   const existingHash = record.value.match(DNSLINK_REGEX)[1]
-
   // get the new ipfs hash from pinata and pin
   const newHash = await pinata
     .pinFromFS(path.join(__dirname, 'out'), { pinataMetadata: { name: PINATA_NAME } })
@@ -30,13 +31,11 @@ async function main() {
       console.log('Successfully pinned new files', hash)
       return hash
     })
-
   if (newHash !== existingHash) {
     // stop pinning the existing ipfs hash on pinata
     await pinata.removePinFromIPFS(existingHash).then(() => {
       console.log('Successfully unpinned existing files')
     })
-
     // delete the existing dnslink record from zeit
     await request
       .delete(`https://api.zeit.co/v2/domains/${DOMAIN}/records/${record.id}`, {
@@ -48,7 +47,6 @@ async function main() {
       .then(() => {
         console.log('Successfully deleted existing DNS record')
       })
-
     // add the new dnslink record to zeit
     await request
       .post(`https://api.zeit.co/v2/domains/${DOMAIN}/records`, {
